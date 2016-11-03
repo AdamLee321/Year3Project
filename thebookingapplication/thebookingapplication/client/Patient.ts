a5da1ffecc521a4bf65bea051b661768e01aafcb
James Bryan <S00153141@mail.itsligo.ie>
2016-11-03 15:47:02 +0000

    PatientFiles

---
+++ thebookingapplication/thebookingapplication/client/Patient.ts
@@ -0,0 +1,12 @@
+export class Patient{
+	_id : number;
+	userID: number;
+	firstname: string;
+	suName: string;
+	Address1: string;
+	Address2: string;
+	City: string;
+	County: string;
+	email: string;
+	phonenumber: string;
+}
